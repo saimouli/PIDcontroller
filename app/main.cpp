d1fbb4e8823fa577acb2f836294a7a51dfdb9f85
kapil <kapilrawal1995@gmail.com>
2018-09-24 13:50:47 -0400

    Minor fixes in main

--- app/main.cpp
+++ app/main.cpp
@@ -16,20 +16,14 @@ using std::cout;
 using std::endl;
 
 int main() {
-    //PID pid(2, 1, 0.1, 0.5);  // this is sai's test value
-  //PID pid(0.1, 0.01, 0.5, 0.1);
-
   PID pid(2, 1, 0.1, 0.5);
   float fVal, iVal;
   cout << "enter the (set point)final velocity" << endl;
   cin >> fVal;
   cout << "enter the initial velocity" << endl;
   cin >> iVal;
-  for (int i = 0; i < 1; i++) {
-    float inc = pid.computePID(fVal, iVal);
-    cout << inc << endl;
-    //printf("val:% 7.3f inc:% 7.3f\n", val, inc);
-    // iVal += inc;
-  }
-    return 0;
+  float inc = pid.computePID(fVal, iVal);
+  cout << inc << endl;
+   
+  return 0;
 }
